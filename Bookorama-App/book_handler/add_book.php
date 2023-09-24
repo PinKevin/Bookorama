d312898569bbd969d014562b98dd02d4baa7d408
PinKevin <emeriokevin2320@gmail.com>
2023-09-24 10:53:13 +0700

    Mengedit add_book untuk pengecekan double ISBN dan hold data yang sudah masuk

--- Bookorama-App/book_handler/add_book.php
+++ Bookorama-App/book_handler/add_book.php
@@ -10,12 +10,32 @@ $errors = array(
     'category' => ''
 );
 
+function isbn_exists_in_database($isbn)
+{
+    global $db; // Variabel koneksi ke database
+
+    $query = "SELECT COUNT(*) AS count FROM books WHERE isbn = '$isbn'";
+    $result = $db->query($query);
+
+    if (!$result) {
+        die('Could not query the database: <br/>' . $db->error . '<br>Query:' . $query);
+    }
+
+    $row = $result->fetch_assoc();
+    $count = $row['count'];
+
+    return $count > 0;
+}
+
 if (isset($_POST['submit'])) {
     $valid = true;
 
     $isbn = test_input($_POST['isbn']);
     if (empty($isbn)) {
         $errors['isbn'] = 'ISBN is required';
+        $valid = false;
+    } elseif (isbn_exists_in_database($isbn)) {
+        $errors['isbn'] = 'ISBN already exists in the database';
         $valid = false;
     }
 
@@ -72,28 +92,28 @@ if (isset($_POST['submit'])) {
         <form action="add_book.php" method="POST" autocomplete="on">
             <div class="form-group">
                 <label for="isbn">ISBN:</label>
-                <input type="text" class="form-control" id="isbn" name="isbn">
+                <input type="text" class="form-control" id="isbn" name="isbn" value="<?= isset($_POST['isbn']) ? htmlspecialchars($_POST['isbn']) : '' ?>">
                 <?php if (!empty($errors['isbn'])) : ?>
                     <div class="alert alert-danger"><?= $errors['isbn'] ?></div>
                 <?php endif; ?>
             </div>
             <div class="form-group">
                 <label for="author">Author:</label>
-                <input type="text" class="form-control" id="author" name="author">
+                <input type="text" class="form-control" id="author" name="author" value="<?= isset($_POST['author']) ? htmlspecialchars($_POST['author']) : '' ?>">
                 <?php if (!empty($errors['author'])) : ?>
                     <div class="alert alert-danger"><?= $errors['author'] ?></div>
                 <?php endif; ?>
             </div>
             <div class="form-group">
                 <label for="title">Title:</label>
-                <input type="text" class="form-control" id="title" name="title">
+                <input type="text" class="form-control" id="title" name="title" value="<?= isset($_POST['title']) ? htmlspecialchars($_POST['title']) : '' ?>">
                 <?php if (!empty($errors['title'])) : ?>
                     <div class="alert alert-danger"><?= $errors['title'] ?></div>
                 <?php endif; ?>
             </div>
             <div class="form-group">
                 <label for="price">Price:</label>
-                <input type="number" class="form-control" id="price" name="price" step="0.01">
+                <input type="number" class="form-control" id="price" name="price" step="0.01" value="<?= isset($_POST['price']) ? htmlspecialchars($_POST['price']) : '' ?>">
                 <?php if (!empty($errors['price'])) : ?>
                     <div class="alert alert-danger"><?= $errors['price'] ?></div>
                 <?php endif; ?>
@@ -109,7 +129,8 @@ if (isset($_POST['submit'])) {
                     if ($result) {
                         while ($row = $result->fetch_assoc()) {
                             $categoryName = $row['name'];
-                            echo "<option value=\"$categoryName\">$categoryName</option>";
+                            $isSelected = isset($_POST['category']) && $_POST['category'] === $categoryName ? 'selected' : '';
+                            echo "<option value=\"$categoryName\" $isSelected>$categoryName</option>";
                         }
 
                         $result->free_result();
