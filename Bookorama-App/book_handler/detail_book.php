253c53cb3132d4fbc78e37d995718eb519ea9c96
PlattenEcho <akunsekolahdika@gmail.com>
2023-09-24 22:02:22 +0700

    dika

--- Bookorama-App/book_handler/detail_book.php
+++ Bookorama-App/book_handler/detail_book.php
@@ -38,25 +38,6 @@ if (!isset($_POST['submit'])) {
             header('Location: detail_book.php?isbn=' . $isbn);
         }
     }
-    // if ($valid) {
-    //     $query = "UPDATE books 
-    //                 SET isbn = '" . $isbn . "', 
-    //                     author = '" . $author . "',
-    //                     title = '" . $title . "',
-    //                     price = '" . $price . "',
-    //                     categoryid = (SELECT categoryid FROM categories WHERE name = '" . $category . "')
-    //                 WHERE isbn = '" . $isbn . "'
-    //             ";
-    //     $result = $db->query($query);
-    //     if (!$result) {
-    //         die("Could not query the database: <br />" . $db->error . '<br>Query: ' . $query);
-    //     } else {
-    //         $db->close();
-    //         header('Location: view_books.php');
-    //     }
-    // }
-
-
 }
 
 ?>
@@ -72,37 +53,38 @@ if (!isset($_POST['submit'])) {
                 <div class="error">
                     <?php if (isset($error_isbn))
                         echo $error_isbn ?>
+                    </div>
                 </div>
-            </div>
-            <div class="form-group">
-                <label for="author">Author:</label>
-                <input type="text" class="form-control" id="author" name="author" value="<?= $author; ?>" disabled>
+                <div class="form-group">
+                    <label for="author">Author:</label>
+                    <input type="text" class="form-control" id="author" name="author" value="<?= $author; ?>" disabled>
                 <div class="error">
                     <?php if (isset($error_author))
                         echo $error_author ?>
+                    </div>
                 </div>
-            </div>
-            <div class="form-group">
-                <label for="title">Title:</label>
-                <input type="text" class="form-control" id="title" name="title" value="<?= $title; ?>" disabled>
+                <div class="form-group">
+                    <label for="title">Title:</label>
+                    <input type="text" class="form-control" id="title" name="title" value="<?= $title; ?>" disabled>
                 <div class="error">
                     <?php if (isset($error_title))
                         echo $error_title ?>
+                    </div>
                 </div>
-            </div>
-            <div class="form-group">
-                <label for="price">Price:</label>
-                <input type="number" class="form-control" id="price" name="price" step="0.01" value="<?= $price; ?>" disabled>
+                <div class="form-group">
+                    <label for="price">Price:</label>
+                    <input type="number" class="form-control" id="price" name="price" step="0.01" value="<?= $price; ?>"
+                    disabled>
                 <div class="error">
                     <?php if (isset($error_price))
                         echo $error_price ?>
+                    </div>
                 </div>
-            </div>
-            <div class="form-group">
-                <label for="category">Category:</label>
-                <select name="category" id="category" class="form-control" disabled>
-                    <option value="" selected disabled>--Select a Category--</option>
-                    <?php
+                <div class="form-group">
+                    <label for="category">Category:</label>
+                    <select name="category" id="category" class="form-control" disabled>
+                        <option value="" selected disabled>--Select a Category--</option>
+                        <?php
                     $query = 'SELECT name FROM categories';
                     $result = $db->query($query);
 
@@ -123,43 +105,43 @@ if (!isset($_POST['submit'])) {
                 <div class="error">
                     <?php if (isset($error_category))
                         echo $error_category ?>
+                    </div>
                 </div>
-            </div>
-            <br>
-            <h5>Reviews <span class="badge badge-secondary">New</span></h5>
-            <table class="table table-striped">
-                <tr>
-                    <th>No.</th>
-                    <th>Review</th>
-                </tr>
+                <br>
+                <h5>Reviews <span class="badge badge-secondary">New</span></h5>
+                <table class="table table-striped">
+                    <tr>
+                        <th>No.</th>
+                        <th>Review</th>
+                    </tr>
 
-                <?php
-                // TODO 1: Tuliskan dan eksekusi query
-                $getReviews = "SELECT a.isbn, a.review FROM book_reviews a WHERE a.isbn='" . $isbn . "'";
-                $reviews = $db->query($getReviews);
-                if (!$reviews) {
-                    die('Could not query the database: <br/>' . $db->error . '<br>Query:' . $getReviews);
-                }
+                    <?php
+                    $getReviews = "SELECT a.isbn, a.review FROM book_reviews a WHERE a.isbn='" . $isbn . "'";
+                    $reviews = $db->query($getReviews);
+                    if (!$reviews) {
+                        die('Could not query the database: <br/>' . $db->error . '<br>Query:' . $getReviews);
+                    }
 
-                $i = 1;
-                while ($row = $reviews->fetch_object()) {
-                    echo '<tr>';
-                    echo '<td>' . $i . '</td>';
-                    echo '<td>' . $row->review . '</td>';
-                    echo '</tr>';
-                    $i++;
-                }
-                // echo 'Total Reviews = ' . $reviews->num_rows;
+                    $i = 1;
+                    while ($row = $reviews->fetch_object()) {
+                        echo '<tr>';
+                        echo '<td>' . $i . '</td>';
+                        echo '<td>' . $row->review . '</td>';
+                        echo '</tr>';
+                        $i++;
+                    }
 
-                $reviews->free();
-                ?>
+                    $reviews->free();
+                    ?>
             </table>
 
             <div class="form-group">
                 <label for="author">Write Review:</label>
                 <input type="text" class="form-control" id="review" name="review" value="">
-                <?php if (!empty($error_rev)) : ?>
-                    <div class="alert alert-danger"><?= $error_rev ?></div>
+                <?php if (!empty($error_rev)): ?>
+                    <div class="alert alert-danger">
+                        <?= $error_rev ?>
+                    </div>
                 <?php endif; ?>
             </div>
             <br />
